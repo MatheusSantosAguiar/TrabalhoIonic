d25647b156f2c462dda6ab836bcacc081cf0d686
MatheusSantosAguiar <matheusaguiar080@gmail.com>
2023-03-25 17:33:03 -0300

    iniciando projeto

---
+++ pao-burger/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PãoBurger',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
