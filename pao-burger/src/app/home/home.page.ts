64c621f171505b0479e896b24c6f0920c8943dc5
MatheusSantosAguiar <matheusaguiar080@gmail.com>
2023-03-26 00:40:41 -0300

    Fazendo alterações

--- pao-burger/src/app/home/home.page.ts
+++ pao-burger/src/app/home/home.page.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
+
 export class HomePage {
 
   constructor() {}
